996c17ee8005536f07083b60fd92d27aba4989ea
Hoa Nguyen <hoanguyen@Hoas-MacBook-Pro.local>
2019-11-22 01:08:49 +0700

    [EDIT] requirements.txt

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+pip install tiobeindexpy
+pip install seaborn
+pip install matplotlib
+pip install numpy
+pip install pandas
+pip install --upgrade beautifulsoup4
+pip install lxml
+pip install thongtinthuoc
