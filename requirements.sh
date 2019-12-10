3bd5c82ba58a847f06c11f53c45e2fbdfec86e5a
Hoa Nguyen <ntthoa.uphcm@gmail.com>
2019-12-11 00:27:47 +0700

    Update README.md, requirements.sh, delete slides.md, videos.md

--- requirements.sh
+++ requirements.sh
@@ -6,3 +6,8 @@ pip install pandas
 pip install --upgrade beautifulsoup4
 pip install lxml
 pip install thongtinthuoc
+pip install ipywidgets==7.5.1
+jupyter nbextension enable --py widgetsnbextension --user
+jupyter nbextension install --py hide_code --user
+jupyter nbextension enable --py hide_code --user
+jupyter serverextension enable --py hide_code --user
