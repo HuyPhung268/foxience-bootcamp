26b39bede4892eaf650dc297e1fb6f6e6f7552b0
Dat Pham <dat@72dpi.co.nz>
2019-12-11 09:16:05 +1300

    add libraries for sql

--- requirements.sh
+++ requirements.sh
@@ -7,6 +7,10 @@ pip install --upgrade beautifulsoup4
 pip install lxml
 pip install thongtinthuoc
 pip install ipywidgets==7.5.1
+
+pip install sqlalchemy # access and query database
+pip install sqlparse # format and pretty print sql query
+
 jupyter nbextension enable --py widgetsnbextension --user
 jupyter nbextension install --py hide_code --user
 jupyter nbextension enable --py hide_code --user
